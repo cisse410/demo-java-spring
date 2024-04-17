01b2d90f4feeca27b62b052f90727d0a8fdee622
cisse410 <dramisse90@gmail.com>
2024-04-17 10:57:12 +0000

    Test function

--- src/test/java/sn/uidt/cisse410/DemoApplicationTests.java
+++ src/test/java/sn/uidt/cisse410/DemoApplicationTests.java
@@ -1,13 +1,28 @@
 package sn.uidt.cisse410;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import sn.uidt.cisse410.service.BusinessService;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	BusinessService bs;
+
 	@Test
 	void contextLoads() {
+	}
+
+	public void testGetWelcome() {
+		String expected = "A demo java spring application";
+
+		String result = bs.getWelcome().getValue();
+		assertEquals(expected, result);
 	}
 
 }
